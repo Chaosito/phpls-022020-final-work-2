9b6a06de27946ef26bc6f02b43aacdac368d83d5
Sergey Aleinikov <chaositomail@gmail.com>
2020-03-08 20:22:06 +0600

    Вынос протокола и домена ImageFaker'a в константы Фиксы от наставника.

--- core/ImageFaker.php
+++ core/ImageFaker.php
@@ -5,11 +5,17 @@ use Intervention\Image\ImageManagerStatic;
 
 class ImageFaker
 {
+    // https, http, ftp, etc
+    const FAKER_DOMAIN_PROTOCOL = 'http';
+    const FAKER_DOMAIN = 'placeimg.com';
+
     private $objImagick;
 
     public function __construct($imageWidth, $imageHeight)
     {
-        $this->objImagick = ImageManagerStatic::make("http://placeimg.com/{$imageWidth}/{$imageHeight}/any");
+        $this->objImagick = ImageManagerStatic::make(
+            self::FAKER_DOMAIN_PROTOCOL."://".self::FAKER_DOMAIN."/{$imageWidth}/{$imageHeight}/any"
+        );
     }
 
     public function saveTo($toPath)
