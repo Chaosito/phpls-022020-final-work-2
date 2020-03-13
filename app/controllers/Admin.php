cafb19e715ff295bff1da424b79ed144a7b1d907
Sergey Aleinikov <chaositomail@gmail.com>
2020-03-13 14:47:26 +0600

    Admin controller

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -1,10 +1,16 @@
 <?php
 namespace app\controllers;
 
+use core\util\globals\AdapterGet;
+use core\util\globals\GlobalsFactory;
+
 class Admin extends \core\Controller
 {
     public function indexAction()
     {
-        $this->pageTitle = 'Admin page';
+        $this->pageTitle = 'Admin page';        GlobalsFactory::init(new AdapterGet());
+        $users = \app\emodels\User::query()->orderBy('id')->get();
+
+        $this->view->users = $users;
     }
 }
